9a0f0b9c739d3f756496fb77d69bf8f502e7f9e0
HJR <HJR@DESKTOP-41I1JEU>
2017-03-02 14:50:01 -0600

    Add project files.

---
+++ Sesion2TelmexHub/Entidades/Automovil.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion2TelmexHub.Entidades
+{
+    public enum Colores
+    {
+        Rojo,
+        Plata,
+        Azul,
+        Negro,
+        Amarillo
+    }
+
+    public enum Estados
+    {
+        Encendido=10,
+        Apagado=20
+    }
+
+    public class Automovil
+    {
+        //http://collabedit.com/bgvuy
+
+        private Colores color;
+
+        public Colores Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        private Estados estadoDeArranque;
+
+        public Estados EstadoDeArranque
+        {
+            get { return estadoDeArranque; }
+            set { estadoDeArranque = value; }
+        }
+
+        private Guid numeroDeSerie;
+
+        public Guid NumeroDeSerie
+        {
+            get { return numeroDeSerie; }
+        
+        }
+
+        private string tamaño;
+
+        public string Tamaño
+        {
+            get { return tamaño; }
+            set { tamaño = value; }
+        }
+
+        private string marca;
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        private string subMarca;
+
+        public string SubMarca
+        {
+            get { return subMarca; }
+            set { subMarca = value; }
+        }
+
+        private int numeroDePasajeros;
+
+        public int NumeroDePasajeros
+        {
+            get { return numeroDePasajeros; }
+            set
+            {
+                if (value < 10)
+                {
+                    numeroDePasajeros = value;
+                }
+                else
+                {
+                    numeroDePasajeros = -1;
+                }
+            }
+        }
+
+        public Automovil()
+        {
+             numeroDeSerie = Guid.NewGuid();
+        }
+
+        public void ProducirAutomovil()
+        {
+            NumeroDePasajeros = 10;
+            Color = Colores.Plata;
+            Marca = "Ford";
+            Tamaño = "Compacto";
+            imprimirAutomovil();
+        }
+
+        public void ProducirAutomovil(int numeroDePasajeros)
+        {
+            NumeroDePasajeros = numeroDePasajeros;
+            Color = Colores.Plata;
+            Marca = "Ford";
+            Tamaño = "Compacto";
+            imprimirAutomovil();
+        }
+
+        public void ProducirAutomovil(int numeroDePasajeros, Colores color)
+        {
+            NumeroDePasajeros = numeroDePasajeros;
+            Color = color;
+            Marca = "Ford";
+            Tamaño = "Compacto";
+            imprimirAutomovil();
+        }
+
+        public void ApagarAuto()
+        {
+            Console.WriteLine("El auto esta Apagado");
+        }
+
+
+
+        public void ProducirAutomovil(Colores color)
+        {
+            NumeroDePasajeros = 4;
+            Color = color;
+            Marca = "Ford";
+            Tamaño = "Compacto";
+            imprimirAutomovil();
+           
+        }
+
+        public void Encender()
+        {
+            estadoDeArranque = Estados.Encendido;
+            imprimirEstado();
+
+        }
+
+        public void Apagar()
+        {
+            estadoDeArranque = Estados.Apagado;
+            imprimirEstado();
+        }
+
+        protected void imprimirEstado()
+        {
+            Console.WriteLine($"Auto: { estadoDeArranque}");
+        }
+
+        private void imprimirAutomovil()
+        {
+            Console.WriteLine
+              ($"Número de serie: {NumeroDeSerie}");
+            Console.WriteLine
+               ($"Número de pasajeros: {NumeroDePasajeros}");
+            Console.WriteLine
+               ($"Marca: {Marca}");
+            Console.WriteLine
+               ($"Tamaño: {Tamaño}");
+            Console.WriteLine
+               ($"Color: {Color}");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Sesion2TelmexHub/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sesion2TelmexHub.Entidades;
+
+namespace Sesion2TelmexHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repetir=true;
+            while (repetir)
+            {
+                Console.Clear();
+                Console.WriteLine("Introduce el número de pasajeros que deseas ");
+                string numeroDePasajerosTexto = Console.ReadLine();
+                int numeroDePasajeros = 0;
+                Automovil MiPrimerAutomovil = new Automovil();
+                if (int.TryParse(numeroDePasajerosTexto, out numeroDePasajeros))
+                {
+                    Console.WriteLine("Introduce el color  que deseas ");
+                    string color = Console.ReadLine();
+                    Colores colorSeleccionado;
+                    if (Enum.TryParse(color, out colorSeleccionado))
+                    {
+                        switch (colorSeleccionado)
+                        {
+                            case Colores.Rojo:
+                            case Colores.Plata:
+                            case Colores.Azul:
+                                MiPrimerAutomovil.ProducirAutomovil(numeroDePasajeros, colorSeleccionado);
+                                break;
+                            case Colores.Negro:
+                                Console.WriteLine("No en este color disponible");
+                                break;
+                            default:
+                                Console.WriteLine("Color no válido");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error al seleccionar el color");
+                    }
+                    //Colores colorSeleccionado = (Colores)Enum.Parse(typeof(Colores), color);
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Error en número de pasajeros");
+                }
+
+
+                //if(VariableTipoBool) if(miBool)
+                //if(invocacionAMetodo) if(miPrimerAutomovil.ValidarNumero(numero))
+                //if(lineaDeCodigo) if(numeroDePasajeros==4) < > <= >=
+
+                Console.WriteLine("Desea seleccionar otro Automovil S/N");
+                var continuar = Console.ReadLine();
+                if (continuar.ToUpper() != "S") repetir = false;
+                 
+            }
+           Console.ReadKey();
+        }
+    }
+}
